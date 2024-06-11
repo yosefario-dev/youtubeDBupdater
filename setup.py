60ac56d7a994ed538ea9d32ef625a21ae2935bf1
Yosefario <168045758+yosefario-dev@users.noreply.github.com>
2024-06-11 12:29:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def setup_channels_file():
         with open("channels.txt", "w") as file:
             file.write("# Add your YouTube channel links here, one per line\n")
             file.write("# Example:\n")
-            file.write("# https://www.youtube.com/user/example_channel\n")
+            file.write("# https://www.youtube.com/@example_channel\n")
 
 def print_application_info():
     print("YouTube Channel Updater Setup")
